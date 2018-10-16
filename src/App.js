57ec4e63db026c760395899fb8ad7a544df7d483
Bartosz Cytrowski <bartosz.cytrowski@gmail.com>
2018-10-16 19:26:12 +0200

    Task list layout and basic JSX done

--- src/App.js
+++ src/App.js
@@ -1,26 +1,78 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <section className="todoapp">
+        <div>
+          <header className="header">
+            <h1>todos</h1>
+            <input
+              className="new-todo"
+              placeholder="What needs to be done?"
+              value=""
+            />
+          </header>
+          <section className="main">
+            <input id="toggle-all" className="toggle-all" type="checkbox" />
+            <label for="toggle-all" />
+            <ul className="todo-list">
+              <li className="completed">
+                <div className="view">
+                  <input className="toggle" type="checkbox" />
+                  <label>Be awesome</label>
+                  <button className="destroy" />
+                </div>
+                <input className="edit" value="Be awesome" />
+              </li>
+              <li className="editing">
+                <div className="view">
+                  <input className="toggle" type="checkbox" />
+                  <label>Go home</label>
+                  <button className="destroy" />
+                </div>
+                <input className="edit" value="Go home" />
+              </li>
+              <li className="">
+                <div className="view">
+                  <input className="toggle" type="checkbox" />
+                  <label>Learn React</label>
+                  <button className="destroy" />
+                </div>
+                <input className="edit" value="Learn React" />
+              </li>
+            </ul>
+          </section>
+          <footer className="footer">
+            <span className="todo-count">
+              <strong>2</strong>
+              <span> </span>
+              <span>items</span>
+              <span> left</span>
+            </span>
+            <ul className="filters">
+              <li>
+                <a href="#/" className="selected">
+                  All
+                </a>
+              </li>
+              <span> </span>
+              <li>
+                <a href="#/active" className="">
+                  Active
+                </a>
+              </li>
+              <span> </span>
+              <li>
+                <a href="#/completed" className="">
+                  Completed
+                </a>
+              </li>
+            </ul>
+            <button className="clear-completed">Clear completed</button>
+          </footer>
+        </div>
+      </section>
     );
   }
 }
